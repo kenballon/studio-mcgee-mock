f89128532c474b2c134451ace28b340d0fe336a2
kenballon <49665487+kenballon@users.noreply.github.com>
2022-01-20 17:17:56 +0800

    deployment Jan 20

---
+++ dist/js/about.18bc12c3.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(n,t,a){"use strict";a.r(t);var e=a("7a23"),c={class:"about"},o=Object(e["h"])("h1",null,"This is an about page",-1);function s(n,t){return Object(e["q"])(),Object(e["e"])("div",c,[o])}var u=a("6b0d"),b=a.n(u);const i={},r=b()(i,[["render",s]]);t["default"]=r}}]);
+//# sourceMappingURL=about.18bc12c3.js.map
